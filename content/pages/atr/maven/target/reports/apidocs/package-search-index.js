c8a041fe0a861182c0f14873c5e958f594bc4f4f
Hervé Boutemy <hboutemy@apache.org>
2026-05-26 18:46:35 +0200

    test publish atr-maven-plugin doc as html

---
+++ content/pages/atr/maven/target/reports/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.tooling.atr"},{"l":"org.apache.tooling.atr_maven_plugin"},{"l":"org.apache.tooling.atr.client"}];updateSearchResults();
